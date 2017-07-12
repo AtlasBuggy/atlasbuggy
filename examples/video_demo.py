f337d2e9e4eb2244a45554180467024dfc7d9ff3
Woz4tetra <woz4tetra@gmail.com>
2017-07-12 18:20:27 -0400

    Removing specific video path

--- examples/video_demo.py
+++ examples/video_demo.py
@@ -6,7 +6,7 @@ from atlasbuggy.cameras.videoplayer import VideoPlayer
 robot = Robot()
 
 viewer = CameraViewerWithTrackbar()
-video = VideoPlayer(file_name="/Users/Woz4tetra/Google Drive/Atlas Docs/Media/Videos/Naboris Demo.mp4", width=800, height=500)
+video = VideoPlayer(file_name="...", width=800, height=500)
 
 viewer.subscribe(Update(viewer.capture_tag, video))
 
