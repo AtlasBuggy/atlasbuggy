815bd9a3f99a611539339c83eb7e55324943368e
Woz4tetra <woz4tetra@gmail.com>
2017-10-27 00:02:36 -0400

    Added a name property to messages similar to nodes

--- message.py
+++ message.py
@@ -5,12 +5,13 @@ import time
 class Message:
     message_regex = r"Message\(t=(\d.*), n=(\d*)\)"
 
-    def __init__(self, timestamp=None, n=0):
+    def __init__(self, timestamp=None, n=0, name=None):
         if timestamp is None:
             self.timestamp = time.time()
         else:
             self.timestamp = timestamp
         self.n = n
+        self._name = name
 
     @classmethod
     def parse(cls, message):
@@ -23,5 +24,12 @@ class Message:
         else:
             return None
 
+    @property
+    def name(self):
+        if not hasattr(self, "_name") or self._name is None:
+            return self.__class__.__name__
+        else:
+            return self._name
+
     def __str__(self):
-        return "%s(t=%s, n=%s)" % (self.__class__.__name__, self.timestamp, self.n)
+        return "%s(t=%s, n=%s)" % (self.name, self.timestamp, self.n)
