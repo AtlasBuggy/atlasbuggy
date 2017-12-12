f0baec1b815e9bd9e0f15c7478326b1100dbd1b6
Woz4tetra <woz4tetra@gmail.com>
2017-12-11 20:23:02 -0500

    Added stereo camera message

--- opencv/messages.py
+++ opencv/messages.py
@@ -1,10 +1,11 @@
 import numpy as np
+
 try:
     import cv2
+
     opencv_installed = True
 except ImportError:
     opencv_installed = False
-
 
 from atlasbuggy.message import Message
 
@@ -17,16 +18,21 @@ class ImageMessage(Message):
         self.image = image
 
         if width is None or height is None:
-            self.height, self.width, = self.image.shape[:2]
-            if len(self.image.shape) == 3:
-                self.depth = self.image.shape[2]
-        else:
-            self.width = width
-            self.height = height
-            self.depth = depth
+            width, height, depth = self.get_image_dimensions(image)
 
+        self.width = width
+        self.height = height
+        self.depth = depth
 
         super(ImageMessage, self).__init__(timestamp, n)
+
+    def get_image_dimensions(self, image):
+        height, width, = image.shape[:2]
+        if len(image.shape) == 3:
+            depth = image.shape[2]
+        else:
+            depth = 1
+        return width, height, depth
 
     def __str__(self):
         return "%s(t=%s, n=%s)" % (self.__class__.__name__, self.timestamp, self.n)
@@ -43,3 +49,22 @@ class ImageMessage(Message):
             return cv2.imencode(".jpg", self.image)[1].tostring()
         else:
             raise ImportError("OpenCV not installed. Can't convert image")
+
+
+class StereoImageMessage(ImageMessage):
+    def __init__(self, left_image, right_image, n, separation_dist, width=None, height=None, depth=3, timestamp=None):
+        self.left_image = left_image
+        self.right_image = right_image
+
+        self.separation_dist = separation_dist
+
+        super(StereoImageMessage, self).__init__(left_image, n, width, height, depth, timestamp)
+
+        width, height, depth = self.get_image_dimensions(self.right_image)
+
+        assert width == self.width, \
+            "Width of left image (%s) does not match right image (%s)!!" % (self.width, width)
+        assert height == self.height, \
+            "Height of left image (%s) does not match right image (%s)!!" % (self.height, height)
+        assert depth == self.depth, \
+            "Depth of left image (%s) does not match right image (%s)!!" % (self.depth, depth)
